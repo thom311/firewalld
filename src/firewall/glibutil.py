a0bbebac4abd0f9ecdff59f51b96a44177f29e31
Thomas Haller <thaller@redhat.com>
2023-07-21 11:34:30 +0200

    improvement(glibutil): add firewall.glibutil.timeout helper for keeping track of timers
    When we schedule a timer, we must always keep track of it so that it can
    be cancelled or restarted.
    
    firewalld is centered around the GLib mainloop via `dbus.mainloop.glib`.
    The new API uses GLib timers and GMainContext for the timeouts. It is
    thus placed in a new, GLib-dependent module "firewall.glibutil".
    
    That GLib timer API returns a handle (a number) with which we can cancel
    the timeout later. The number is slightly inconvenient, because we must
    never cancel a timer that was already cancelled or invoked. That could
    be avoided by using GSource instances instead of the numeric source IDs.
    Still, it means that the caller needs to keep track of the handle.
    
    The new API allows to instead pass key and tags, to cancel timers
    without need to keeping track of the handle. The key uniquely identifies
    the timer, and a caller might know in advance a unique key they can use,
    instead of tracking the handle. Additionally, tags allow to bulk cancel
    timers. These features should make it simpler to manage pending timers,
    and allow to forget about the handle. Keeping track of the handle means
    to keep track of another piece of state, instead let the "timeout" API
    keep track of it.
    
    The use case is that we schedule timeouts for a zone's service. So we
    can
    
       timeout.schedule(timeout, callback, key=('service', zone_name, service_name))
    
    using a caller-chosen key that we can later also use for cancellation. We can also
    pass tags, to bulk cancel timers when the zone/service gets deleted, via
    
       timeout.schedule(timeout,
                        callback,
                        key=('service', zone_name, service_name),
                        tags=(('zone', zone_name), ('service', service_name)))

---
+++ src/firewall/glibutil.py
@@ -0,0 +1,207 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from gi.repository import GLib
+
+
+###############################################################################
+
+
+class _Timeout:
+    class _Handle:
+        def __init__(handle, self, key, timeout, callback, tags, timeout_exact):
+            if tags is None:
+                tags = ()
+            else:
+                tags = tuple(dict.fromkeys(tags))
+
+            if key is None:
+                # If the caller provided no key, the timeout cannot
+                # be cancelled via the key. The internally used key,
+                # is the handle instance itself.
+                key = handle
+
+            handle.self = self
+            handle.key = key
+
+            handle.callback = callback
+            handle.tags = tags
+            handle._gsourceid_id = 0
+
+            for tag in tags:
+                handle._tags_register(tag)
+            self._handles[key] = handle
+            handle._gsourceid_attach(timeout, timeout_exact)
+
+        def _destroy(handle):
+            self = handle.self
+            handle._gsourceid_remove()
+            del self._handles[handle.key]
+            for tag in handle.tags:
+                handle._tags_unregister(tag)
+
+        def _reschedule(handle, timeout, callback, tags, replace_tags, timeout_exact):
+            add_tags = True
+            if replace_tags or not handle.tags:
+                if tags is None:
+                    new_tags = ()
+                else:
+                    new_tags = dict.fromkeys(tags)
+            else:
+                if tags is None:
+                    new_tags = handle.tags
+                    add_tags = False
+                else:
+                    new_tags = dict.fromkeys(handle.tags)
+                    for t in tags:
+                        new_tags[t] = None
+
+            # `new_tags` is either a (unique) tuple or a dict.  Both is fine, we
+            # will convert it to a tuple for `handle.tags`.
+            if add_tags:
+                for tag in new_tags:
+                    handle._tags_register(tag)
+            if replace_tags:
+                for tag in handle.tags:
+                    # At this place, `new_tags` is either an empty tuple or a
+                    # set. So the entire loop is O(n).
+                    if tag not in new_tags:
+                        handle._tags_unregister(tag)
+
+            handle.callback = callback
+            handle.tags = tuple(new_tags)
+
+            handle._gsourceid_remove()
+            handle._gsourceid_attach(timeout, timeout_exact)
+
+        def _gsourceid_remove(handle):
+            if handle._gsourceid_id != 0:
+                GLib.source_remove(handle._gsourceid_id)
+                handle._gsourceid_id = 0
+
+        def _gsourceid_attach(handle, timeout, timeout_exact):
+            if timeout_exact or isinstance(timeout, float):
+                timeout = timeout * 1000.0
+                if timeout < 1:
+                    timeout = 1
+                elif timeout > 0xFFFFFFFF:
+                    timeout = 0xFFFFFFFF
+                sid = GLib.timeout_add(int(timeout), handle._timeout_cb)
+            else:
+                if timeout > 0xFFFFFFFF:
+                    timeout = 0xFFFFFFFF
+                sid = GLib.timeout_add_seconds(timeout, handle._timeout_cb)
+            handle._gsourceid_id = sid
+
+        def _timeout_cb(handle):
+            callback = handle.callback
+            handle._gsourceid_id = 0
+            handle._destroy()
+            try:
+                callback()
+            except Exception:
+                import traceback
+
+                traceback.print_exc()
+            return False
+
+        def _tags_register(handle, tag):
+            # We allow that handle may or may not be registered already.
+            self = handle.self
+            s = self._tags.get(tag)
+            if s is None:
+                s = set()
+                self._tags[tag] = s
+            s.add(handle)
+
+        def _tags_unregister(handle, tag):
+            # We expect that handle is registered under tag.
+            self = handle.self
+            s = self._tags[tag]
+            s.remove(handle)
+            if not s:
+                del self._tags[tag]
+
+    def __init__(self):
+        self._handles = {}
+        self._tags = {}
+
+    def _parse_timeout(self, timeout):
+        timeout0 = timeout
+        if isinstance(timeout, str):
+            try:
+                timeout = int(timeout)
+            except ValueError:
+                try:
+                    timeout = float(timeout)
+                except ValueError:
+                    timeout = None
+        elif isinstance(timeout, int) or isinstance(timeout, float):
+            pass
+        else:
+            timeout = None
+
+        if timeout is None:
+            raise TypeError(f"timeout expects a number but is '{timeout0}'")
+        if timeout < 0:
+            raise TypeError("timeout cannot be negative")
+
+        return timeout
+
+    def schedule(
+        self,
+        timeout,
+        callback,
+        key=None,
+        tags=None,
+        *,
+        replace_tags=True,
+        timeout_exact=False,
+    ):
+        assert callback
+        timeout = self._parse_timeout(timeout)
+
+        if key is not None:
+            handle = self._handles.get(key)
+            if handle is not None:
+                handle._reschedule(timeout, callback, tags, replace_tags, timeout_exact)
+                return handle
+
+        return self._Handle(self, key, timeout, callback, tags, timeout_exact)
+
+    def has(self, key):
+        # This returns the handle instance or None, which also
+        # can be used in a boolean context.
+        return self._handles.get(key)
+
+    def has_handle(self, handle):
+        return (
+            isinstance(handle, self._Handle) and self._handles.get(handle.key) is handle
+        )
+
+    def cancel(self, key):
+        handle = self._handles.get(key)
+        if handle is None:
+            return False
+        handle._destroy()
+        return True
+
+    def cancel_handle(self, handle):
+        if not self.has_handle(handle):
+            return False
+        handle._destroy()
+        return True
+
+    def cancel_tag(self, *tags):
+        n = 0
+        for tag in tags:
+            s = self._tags.get(tag)
+            if s is None:
+                continue
+            s = tuple(s)
+            for handle in s:
+                n += 1
+                handle._destroy()
+        return n
+
+
+timeout = _Timeout()
--- src/tests/python/test_misc.py
+++ src/tests/python/test_misc.py
@@ -1,14 +1,99 @@
 #!/usr/bin/python
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import pytest
+import random
+
+from gi.repository import GLib
+
 import fwtst  # noqa: F401
 
 import firewall.core.icmp
+import firewall.glibutil
 
 
 def test_icmp():
     assert not firewall.core.icmp.check_icmpv6_name("foo")
     assert firewall.core.icmp.check_icmpv6_name("neigbour-solicitation")
+
+
+def test_timeout():
+
+    maincontext = GLib.main_context_default()
+
+    lst_cnt = [0]
+
+    def cb_not_called():
+        raise AssertionError("I should not be called")
+
+    def cb_cnt():
+        lst_cnt[0] += 1
+
+    def cb_cancel(key):
+        firewall.glibutil.timeout.cancel(key)
+
+    firewall.glibutil.timeout.schedule(0.005, lambda: cb_cancel("key2"), "key1")
+    firewall.glibutil.timeout.schedule(0.006, cb_not_called, "key2")
+
+    firewall.glibutil.timeout.schedule(0.01, cb_not_called, "key3")
+    firewall.glibutil.timeout.schedule(0.01, cb_cnt, key="key3")
+
+    h = firewall.glibutil.timeout.schedule(1, cb_not_called, "key4")
+    assert h.key == "key4"
+    assert h.callback == cb_not_called
+    assert h.tags == ()
+    assert firewall.glibutil.timeout.has("key4") is h
+    assert firewall.glibutil.timeout.has("foo") is None
+    assert firewall.glibutil.timeout.cancel("key4")
+    assert firewall.glibutil.timeout.has("key4") is None
+
+    h = firewall.glibutil.timeout.schedule(1, cb_not_called)
+    assert firewall.glibutil.timeout.has_handle(h)
+    assert firewall.glibutil.timeout.cancel_handle(h)
+
+    firewall.glibutil.timeout.schedule(0.001, cb_not_called, tags=("tag1",))
+    assert firewall.glibutil.timeout.cancel_tag("tag1") == 1
+
+    assert not firewall.glibutil.timeout.cancel("keyfoo")
+    assert firewall.glibutil.timeout.cancel_tag("tagfoo") == 0
+
+    while lst_cnt[0] == 0:
+        maincontext.iteration(True)
+
+    assert lst_cnt == [1]
+
+    h = firewall.glibutil.timeout.schedule(1, cb_not_called, "x", tags=["tag1"])
+    assert h.tags == ("tag1",)
+    h2 = firewall.glibutil.timeout.schedule(1, cb_not_called, "x", tags=("tag2",))
+    assert h is h2
+    assert h.tags == ("tag2",)
+    assert firewall.glibutil.timeout.cancel_tag("tag1") == 0
+    assert firewall.glibutil.timeout.cancel_tag("tag2") == 1
+
+    extra_tags = [1, 2, 3, 4]
+    random.shuffle(extra_tags)
+
+    h = firewall.glibutil.timeout.schedule("1", cb_not_called, "x", tags=("tag1",))
+    h2 = firewall.glibutil.timeout.schedule(
+        1, cb_not_called, "x", tags=("tag2",), replace_tags=False
+    )
+    assert h is h2
+    assert h.tags == ("tag1", "tag2")
+    h2 = firewall.glibutil.timeout.schedule(
+        1, cb_not_called, "x", tags=extra_tags, replace_tags=False
+    )
+    assert h is h2
+    assert h.tags == ("tag1", "tag2") + tuple(extra_tags)
+    assert firewall.glibutil.timeout.has("x") is h
+    assert firewall.glibutil.timeout.cancel_tag("tag1") == 1
+    assert firewall.glibutil.timeout.cancel_tag("tag2") == 0
+    assert firewall.glibutil.timeout.has("x") is None
+
+    assert firewall.glibutil.timeout._handles == {}
+    assert firewall.glibutil.timeout._tags == {}
+
+    with pytest.raises(TypeError):
+        firewall.glibutil.timeout.schedule("err", cb_not_called)
 
 
 if __name__ == "__main__":
