a8a886305b108ab37dc0743e36100f10a8455722
Thomas Haller <thaller@redhat.com>
2023-07-21 16:21:31 +0200

    improvement(icmp): drop redundant check_icmptype() functions
    Code does not get simpler, by having aliases that do mostly the same
    and call each other. Calling self._fw.icmp.check_icmptype() is
    perfectly fine. Use that.

--- src/firewall/core/fw_policy.py
+++ src/firewall/core/fw_policy.py
@@ -1075,13 +1075,6 @@ class FirewallPolicy:
 
     # ICMP BLOCK
 
-    def check_icmp_block(self, icmp):
-        self._fw.check_icmptype(icmp)
-
-    def __icmp_block_id(self, icmp):
-        self.check_icmp_block(icmp)
-        return icmp
-
     def add_icmp_block(self, policy, icmp, timeout=0, sender=None,
                        use_transaction=None):
         _policy = self._fw.check_policy(policy)
@@ -1089,7 +1082,7 @@ class FirewallPolicy:
         self._fw.check_panic()
         _obj = self._policies[_policy]
 
-        icmp_id = self.__icmp_block_id(icmp)
+        icmp_id = self._fw.icmptype.check_icmptype(icmp)
         if icmp_id in _obj.icmp_blocks:
             _name = _obj.derived_from_zone if _obj.derived_from_zone else _policy
             raise FirewallError(errors.ALREADY_ENABLED,
@@ -1119,7 +1112,7 @@ class FirewallPolicy:
         self._fw.check_panic()
         _obj = self._policies[_policy]
 
-        icmp_id = self.__icmp_block_id(icmp)
+        icmp_id = self._fw.icmptype.check_icmptype(icmp)
         if icmp_id not in _obj.icmp_blocks:
             _name = _obj.derived_from_zone if _obj.derived_from_zone else _policy
             raise FirewallError(errors.NOT_ENABLED,
@@ -1145,7 +1138,7 @@ class FirewallPolicy:
             _obj.icmp_blocks.remove(icmp_id)
 
     def query_icmp_block(self, policy, icmp):
-        return self.__icmp_block_id(icmp) in self.get_policy(policy).icmp_blocks
+        return self._fw.icmptype.check_icmptype(icmp) in self.get_policy(policy).icmp_blocks
 
     def list_icmp_blocks(self, policy):
         return self.get_policy(policy).icmp_blocks
